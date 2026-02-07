1c5c33a574e0fad15e70998b909607000eae5ef5
rakhmatovsanjar <sanjarbekrahmatov2003@gmail.com>
2026-02-08 03:16:15 +0500

    User message

--- cargo/serializers.py
+++ cargo/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+
+from accounts.models import User
 from .models import Cargo, SupportMessage
 
 
@@ -10,10 +12,25 @@ class CargoSerializer(serializers.ModelSerializer):
 
 class SupportMessageSerializer(serializers.ModelSerializer):
     sender_type = serializers.SerializerMethodField()
+    user = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(),
+        required=False,  # user maydoni majburiy emas
+        allow_null=True  # null qiymatga ruxsat berish
+    )
 
     class Meta:
         model = SupportMessage
-        fields = ['id', 'message', 'image', 'is_from_admin', 'sender_type', 'timestamp_ms']
+        fields = ['id', 'user', 'message', 'image', 'is_from_admin', 'sender_type', 'timestamp_ms']
+        extra_kwargs = {
+            'user': {'required': False, 'allow_null': True}
+        }
 
     def get_sender_type(self, obj):
         return "Admin" if obj.is_from_admin else "Client"
+
+    def create(self, validated_data):
+        # Agar user berilmagan bo'lsa, requestdan olish
+        request = self.context.get('request')
+        if request and request.user and not validated_data.get('user'):
+            validated_data['user'] = request.user
+        return super().create(validated_data)
