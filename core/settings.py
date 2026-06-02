9784f53d05ea96d90603cf073be52add8eb4b945
RakhmatovSanjarbek <sanjarbekrahmatov2003@gmail.com>
2026-06-02 10:50:32 +0500

    Remove whitenoise middleware 1

--- core/settings.py
+++ core/settings.py
@@ -140,7 +140,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
